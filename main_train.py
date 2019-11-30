639d85c8e439a5dc768ad1a7ccd8dd496c75658f
manorz <manor.zvi@campus.technion.ac.il>
2019-11-30 20:08:12 +0200

    SinGAN base + pytorch tutorial for seg

---
+++ main_train.py
@@ -0,0 +1,38 @@
+from config import get_arguments
+from SinGAN.manipulate import *
+from SinGAN.training import *
+import SinGAN.functions as functions
+import shutil
+
+if __name__ == '__main__':
+    parser = get_arguments()
+    parser.add_argument('--input_dir',                      help='input image dir', default='Input/Images')
+    parser.add_argument('--input_name',      required=True, help='input image name')
+    parser.add_argument('--mode',                           help='task to be done', default='train')
+    parser.add_argument('--delete_previous', action='store_true',     help='delete previous run results directory, if exists.', default=False)
+    opt = parser.parse_args()
+    opt = functions.post_config(opt)
+    Gs = []
+    Zs = []
+    reals = []
+    NoiseAmp = []
+    dir2save = functions.generate_dir2save(opt)
+
+    if (os.path.exists(dir2save)):
+        if opt.delete_previous:
+            shutil.rmtree(dir2save)
+        else:
+            print('trained model already exist')
+            exit(1)
+
+    try:
+        os.makedirs(dir2save)
+    except OSError:
+        pass
+
+    real = functions.read_image(opt)
+    functions.adjust_scales2image(real, opt)
+    train(opt, Gs, Zs, reals, NoiseAmp)
+    SinGAN_generate(Gs,Zs,reals,NoiseAmp,opt)
+
+    exit(0)
